3217a6a0a4fb3c4d89229bd07139d07fd3d2a942
Adam <104869908+Adambrouille@users.noreply.github.com>
2023-05-13 14:49:36 +0200

    Add files via upload

---
+++ applecture.js
@@ -0,0 +1,12 @@
+document.getElementById("texteLink").addEventListener("click", function(){
+    document.getElementById("containImgSavoir").style.display = "block";
+  });
+document.getElementById("texteLink").addEventListener("dblclick", function(){
+    document.getElementById("containImgSavoir").style.display = "none";
+  });
+  document.getElementById("LinkQuest").addEventListener("click", function(){
+    document.getElementById("Reponse").style.display = "block";
+  });
+document.getElementById("LinkQuest").addEventListener("dblclick", function(){
+    document.getElementById("Reponse").style.display = "none";
+  });
